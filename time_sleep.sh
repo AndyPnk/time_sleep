da6499a94c8b1785d3b3c146fa6f41df556e09b3
AndyPnk <55383336+AndyPnk@users.noreply.github.com>
2026-07-30 10:15:39 +0300

    Refactor time_sleep.sh to download latest version
    Removed user session checks and gsettings commands for GNOME. Added functionality to download and run the latest version of the script.

--- time_sleep.sh
+++ time_sleep.sh
@@ -2,52 +2,12 @@
 
 set -e
 
-# Пошук активного користувача GNOME
-SESSION=$(loginctl list-sessions --no-legend | awk '$4=="seat0"{print $1; exit}')
-USER_NAME=$(loginctl show-session "$SESSION" -p Name --value)
+URL="https://raw.githubusercontent.com/AndyPnk/time_sleep/main/time_sleep.sh"
 
-if [ -z "$USER_NAME" ]; then
-    echo "Не знайдено активного користувача."
-    exit 1
-fi
+echo "Downloading and running latest version..."
 
-USER_UID=$(id -u "$USER_NAME")
+wget -qO- "$URL" | sudo bash
 
-export XDG_RUNTIME_DIR="/run/user/$USER_UID"
-export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
+RET=$?
 
-run_gsettings() {
-    sudo -u "$USER_NAME" \
-    XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR" \
-    DBUS_SESSION_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS" \
-    gsettings set "$@"
-}
-
-echo "Налаштовую політику енергозбереження для користувача $USER_NAME"
-
-run_gsettings org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 900
-run_gsettings org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type suspend
-
-run_gsettings org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 900
-run_gsettings org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type suspend
-
-run_gsettings org.gnome.desktop.session idle-delay "uint32 900"
-
-echo "idle-delay: $(sudo -u "$USER_NAME" \
-env XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR" \
-DBUS_SESSION_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS" \
-gsettings get org.gnome.desktop.session idle-delay)"
-
-
-run_gsettings org.gnome.desktop.screensaver lock-enabled true
-
-# Для Ubuntu
-if sudo -u "$USER_NAME" \
-    XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR" \
-    DBUS_SESSION_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS" \
-    gsettings writable org.gnome.desktop.screensaver ubuntu-lock-on-suspend >/dev/null 2>&1; then
-
-    run_gsettings org.gnome.desktop.screensaver ubuntu-lock-on-suspend true
-fi
-
-echo "Готово."
+exit $RET
