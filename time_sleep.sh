14cecf73677dd73706fc9e1e2931d000e617d6a4
AndyPnk <55383336+AndyPnk@users.noreply.github.com>
2026-07-30 10:27:13 +0300

    Update time_sleep.sh

--- time_sleep.sh
+++ time_sleep.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
 set -e
-
 URL="https://raw.githubusercontent.com/AndyPnk/time_sleep/main/time_sleep.sh"
-
-echo "Downloading and running latest version..."
-
 wget -qO- "$URL" | sudo bash
-
 RET=$?
-
 exit $RET
