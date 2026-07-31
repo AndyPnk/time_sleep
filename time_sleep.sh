08905bb27da1d926c71d44d97425fcf6a068b980
AndyPnk <55383336+AndyPnk@users.noreply.github.com>
2026-07-31 10:32:01 +0300

    Update time_sleep.sh

--- time_sleep.sh
+++ time_sleep.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
+
 set -e
-URL="https://raw.githubusercontent.com/AndyPnk/time_sleep/main/time_sleep.sh"
-wget -qO- "$URL" | sudo bash
-RET=$?
-exit $RET
+
+USER_NAME=$(loginctl list-sessions --no-legend | awk '$6=="active" {print $3; exit}')
+
+if [ -z "$USER_NAME" ]; then
+    echo 0
+    exit 1
+fi
+
+USER_ID=$(id -u "$USER_NAME")
+
+CURRENT=$(sudo -u "$USER_NAME" \
+DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$USER_ID/bus" \
+gsettings get org.gnome.desktop.session idle-delay)
+
+# Якщо вже встановлено
+if [ "$CURRENT" = "uint32 900" ]; then
+    echo 1
+    exit 0
+fi
+
+
+# Встановлення правила
+sudo -u "$USER_NAME" \
+DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$USER_ID/bus" \
+gsettings set org.gnome.desktop.session idle-delay 900
+
+
+# Перевірка після зміни
+if CHECK=$(sudo -u "$USER_NAME" \
+DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$USER_ID/bus" \
+gsettings get org.gnome.desktop.session idle-delay 2>/dev/null); then
+
+    if [ "$CHECK" = "uint32 900" ]; then
+        echo 1
+    else
+        echo 0
+    fi
+
+else
+    echo 0
+fi
