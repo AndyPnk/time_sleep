f2ce8467aed2c468738f9d05f3798e4d4c749634
AndyPnk <55383336+AndyPnk@users.noreply.github.com>
2026-07-31 10:35:13 +0300

    Update time_sleep.sh

--- time_sleep.sh
+++ time_sleep.sh
@@ -2,7 +2,15 @@
 
 set -e
 
-USER_NAME=$(loginctl list-sessions --no-legend | awk '$6=="active" {print $3; exit}')
+# Знаходимо активну графічну сесію
+SESSION=$(loginctl list-sessions --no-legend | awk '$4=="seat0"{print $1; exit}')
+
+if [ -z "$SESSION" ]; then
+    echo 0
+    exit 1
+fi
+
+USER_NAME=$(loginctl show-session "$SESSION" -p Name --value)
 
 if [ -z "$USER_NAME" ]; then
     echo 0
@@ -12,6 +20,7 @@ fi
 USER_ID=$(id -u "$USER_NAME")
 
 CURRENT=$(sudo -u "$USER_NAME" \
+env XDG_RUNTIME_DIR="/run/user/$USER_ID" \
 DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$USER_ID/bus" \
 gsettings get org.gnome.desktop.session idle-delay)
 
@@ -21,24 +30,20 @@ if [ "$CURRENT" = "uint32 900" ]; then
     exit 0
 fi
 
-
 # Встановлення правила
 sudo -u "$USER_NAME" \
+env XDG_RUNTIME_DIR="/run/user/$USER_ID" \
 DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$USER_ID/bus" \
 gsettings set org.gnome.desktop.session idle-delay 900
 
-
 # Перевірка після зміни
-if CHECK=$(sudo -u "$USER_NAME" \
+CHECK=$(sudo -u "$USER_NAME" \
+env XDG_RUNTIME_DIR="/run/user/$USER_ID" \
 DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$USER_ID/bus" \
-gsettings get org.gnome.desktop.session idle-delay 2>/dev/null); then
+gsettings get org.gnome.desktop.session idle-delay 2>/dev/null)
 
-    if [ "$CHECK" = "uint32 900" ]; then
-        echo 1
-    else
-        echo 0
-    fi
-
+if [ "$CHECK" = "uint32 900" ]; then
+    echo 1
 else
     echo 0
 fi
